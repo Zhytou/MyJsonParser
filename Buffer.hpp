64ce8d9af25be96a3a405db2187b4ebcfc500a3d
zhytou <805007434@qq.com>
2022-05-12 16:34:13 +0800

    fix the ParseContext class

---
+++ Buffer.hpp
@@ -0,0 +1,13 @@
+#ifndef ATOMJSON_BUFFER_HPP
+#define ATOMJSON_BUFFER_HPP
+
+#include <iostream>
+
+#include "Array.hpp"
+
+namespace AtomJson
+{
+    typedef Array Buffer;
+}; // namespace AtomJson
+
+#endif
