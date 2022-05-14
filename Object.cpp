479445da335425f98767ed48b80e6ccfe8c48d54
zhytou <805007434@qq.com>
2022-05-14 21:15:30 +0800

    add Object

--- Object.cpp
+++ Object.cpp
@@ -1,22 +1,80 @@
 #include "AtomJson.hpp"
 #include "Object.hpp"
+#include "DoublyLinkedList.hpp"
+
+#define INITIAL_CAPACITY 16
 
 namespace AtomJson
 {
-    Object::Pair::~Pair()
-    {
-        if (val)
-            delete val;
-    }
-
-    Object::Object(size_t len)
+    Object::Object()
     {
         size = 0;
-        if (len < 10)
-            capacity = 10;
-        else
-            capacity = len;
-        p = new Pair[capacity];
+        occupiedEntry = 0;
+        loadFactor = 0;
+        capacity = INITIAL_CAPACITY;
+        p = new Entry[capacity];
+    }
+
+    Object::Object(const Object &other)
+    {
+        size = other.size;
+        occupiedEntry = other.occupiedEntry;
+        capacity = other.capacity;
+        loadFactor = other.loadFactor;
+
+        p = new Entry[capacity];
+
+        for (size_t i = 0; i < capacity; i++)
+        {
+            p[i] = deepCopyDLL(other.p[i]);
+        }
+    }
+
+    Object::Object(Object &&other)
+    {
+        size = other.size;
+        occupiedEntry = other.occupiedEntry;
+        capacity = other.capacity;
+        loadFactor = other.loadFactor;
+        p = other.p;
+
+        other.size = 0;
+        other.occupiedEntry = 0;
+        other.loadFactor = 0;
+        other.capacity = 0;
+        other.p = nullptr;
+    }
+
+    Object &Object::operator=(const Object &other)
+    {
+        size = other.size;
+        occupiedEntry = other.occupiedEntry;
+        capacity = other.capacity;
+        loadFactor = other.loadFactor;
+
+        p = new Entry[capacity];
+
+        for (size_t i = 0; i < capacity; i++)
+        {
+            p[i] = deepCopyDLL(other.p[i]);
+        }
+        return *this;
+    }
+
+    Object &Object::operator=(Object &&other)
+    {
+        size = other.size;
+        occupiedEntry = other.occupiedEntry;
+        capacity = other.capacity;
+        loadFactor = other.loadFactor;
+        p = other.p;
+
+        other.size = 0;
+        other.occupiedEntry = 0;
+        other.loadFactor = 0;
+        other.capacity = 0;
+        other.p = nullptr;
+        return *this;
     }
 
     Object::~Object()
@@ -25,5 +83,51 @@ namespace AtomJson
             delete[] p;
         size = 0;
         capacity = 0;
+        occupiedEntry = 0;
+    }
+
+    Value &Object::operator[](const String &key)
+    {
+        size_t idx = key.hashcode() % capacity;
+        Node *head = p[idx];
+    }
+
+    bool Object::operator==(const Object &other)
+    {
+        // TODO:
+        return false;
+    }
+
+    void Object::resize()
+    {
+        size_t len = capacity;
+        do
+        {
+            // ? 如何在改变容量的时候计算新情况的加载度？，因为占用entry数量可能会变化
+            if (loadFactor < 0.3)
+                capacity /= 1.5;
+            else if (loadFactor > 0.75)
+                capacity *= 1.5;
+        } while (need_resize());
+
+        occupiedEntry = 0;
+        Node **np = new Entry[capacity];
+        for (size_t i = 0; i < len; i++)
+        {
+            Node *head = p[i];
+            while (head)
+            {
+                size_t idx = head->item.key.hashcode() % capacity;
+                Node *node = new Node();
+                node->item = std::move(p[i]->item);
+                np[idx] = insertDLLNode(np[idx], node);
+                occupiedEntry += 1;
+                head = deleteDLLNode(head);
+            }
+        }
+
+        if (p)
+            delete[] p;
+        p = np;
     }
 } // namespace AtomJson
--- Object.hpp
+++ Object.hpp
@@ -7,19 +7,137 @@ namespace AtomJson
 {
     struct Value;
 
+    struct DoubleyLinkedListNode;
+    typedef DoubleyLinkedListNode Node;
+
     class Object
     {
     public:
-        struct Pair
+        struct Item
         {
             String key;
             Value *val;
-            Pair() : key(), val(nullptr){};
-            ~Pair();
+
+            /**
+             * *Default constructor
+             * @brief Construct a new Item object
+             *
+             */
+            Item() : val(nullptr) {}
+
+            /**
+             * *Copy constructor
+             * @brief Construct a new Item object by copying another Item object
+             *
+             * @param other
+             */
+            Item(const Item &other)
+            {
+                key = other.key;
+                val = new Value(*other.val);
+            }
+
+            /**
+             * *Move constructor
+             * @brief Construct a new Item object by moving another Item object
+             *
+             * @param other
+             */
+            Item(Item &&other)
+            {
+                key = std::move(other.key);
+                val = other.val;
+                other.val = nullptr;
+            }
+
+            /**
+             * *Copy assignment operator
+             * @brief Assign a new Item object by another Item object
+             *
+             * @param other
+             * @return Item&
+             */
+            Item &operator=(const Item &other)
+            {
+                key = other.key;
+                val = new Value(*other.val);
+                return *this;
+            }
+
+            /**
+             * *Move assignment operator
+             * @brief Assign a new Item object by moving another Item object
+             *
+             * @param other
+             * @return Item&
+             */
+            Item &operator=(Item &&other)
+            {
+                key = std::move(other.key);
+                val = other.val;
+                other.val = nullptr;
+                return *this;
+            }
+
+            /**
+             * *Destructor
+             *  @brief Destroy the Item object
+             *
+             */
+            ~Item()
+            {
+                if (val)
+                    delete val;
+            }
         };
 
-        Object(size_t len = 10);
+        /**
+         * *Default constructor
+         * @brief Construct a new Object object
+         *
+         * @param len
+         */
+        Object();
 
+        /**
+         * *Copy constructor
+         * @brief Construct a new Object object
+         *
+         * @param other
+         */
+        Object(const Object &other);
+
+        /**
+         * *Move constructor
+         * @brief Construct a new Object object by moving another Object object
+         *
+         * @param other
+         */
+        Object(Object &&other);
+
+        /**
+         * *Copy assignment operator
+         * @brief
+         *
+         * @param other
+         * @return Object&
+         */
+        Object &operator=(const Object &other);
+
+        /**
+         * *Move assignment operator
+         * @brief
+         *
+         * @param other
+         * @return Object&
+         */
+        Object &operator=(Object &&other);
+
+        /**
+         * *Destructor
+         * @brief Destroy the Object object
+         *
+         */
         ~Object();
 
         /**
@@ -43,11 +161,31 @@ namespace AtomJson
          * @param key
          * @return Value&
          */
-        Value &operator[](String key);
+        Value &operator[](const String &key);
+
+        /**
+         * @brief
+         *
+         * @param other
+         * @return bool
+         */
+        bool operator==(const Object &other);
 
     private:
-        size_t size, capacity;
-        Pair *p;
+        bool need_resize()
+        {
+            loadFactor = 1.0 * occupiedEntry / capacity;
+            return occupiedEntry != 0 && capacity > 16 && (loadFactor < 0.3 || loadFactor > 0.75);
+        }
+        void resize();
+
+        /** size is the number of inserted items. **/
+        size_t size;
+        /** occupied_entry is the number of entries or buckets occupied in the Object.**/
+        /** capacity is the total number of available buckets or entries.**/
+        size_t capacity, occupiedEntry;
+        double loadFactor;
+        Node **p;
     };
 };
 #endif
